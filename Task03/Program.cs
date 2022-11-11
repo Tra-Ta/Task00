d0dc4624d48111b7636f88673aa7283af6b32e6b
Tra-Ta <madtazik@gmail.com>
2022-11-11 22:08:33 +0300

    dd ask03 and ask04

---
+++ Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter an integer --> ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int start = -num;
+if(num < 0) start = num;
+
+int stop = -start;
+
+while(start < stop +1)
+{
+    Console.Write(start + " ");
+    start += 1;
+}
---
+++ Task04/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число -->");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 100 && num < 1000) Console.WriteLine(num % 10);
+else Console.WriteLine("Вне диапозона");
