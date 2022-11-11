2c6fd2eec07c24f3136444c28e09ac6130c6d169
Tra-Ta <madtazik@gmail.com>
2022-11-11 21:41:01 +0300

    dd ask01 and ask02

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 2 числа -->");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");  
+}
